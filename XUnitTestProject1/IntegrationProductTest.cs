fe52148daf4d4aeb36780c237937c35a0dcd0673
davidsantes <30901115+davidsantes@users.noreply.github.com>
2020-11-19 20:06:27 +0100

    Fun - Testing - Añadir tests para get y post

--- XUnitTestProject1/IntegrationProductTest.cs
+++ XUnitTestProject1/IntegrationProductTest.cs
@@ -1,87 +1,89 @@
-﻿using ApiRestful.Service;
-using ApiRestful.Test.ApiTestDtos;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.TestHost;
+﻿using ApiRestful.Test.ApiTestDtos;
+using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Xunit;
 
 namespace ApiRestful.Test
 {
-    //https://www.youtube.com/watch?v=p5l7x_pFjmI
-    //https://docs.microsoft.com/es-es/aspnet/core/test/integration-tests?view=aspnetcore-5.0
-    //https://docs.microsoft.com/es-es/dotnet/architecture/microservices/multi-container-microservice-net-applications/test-aspnet-core-services-web-apps
     public class IntegrationProductTest
-    {
-        private readonly TestServer _server;        
-        private readonly HttpClient _client;
-        private List<ProductDto> sut_list_products;
-
+    {       
         public IntegrationProductTest()
         {
-            // Arrange
-            _server = new TestServer(new WebHostBuilder()
-                   .UseStartup<Startup>());
-            _client = _server.CreateClient();
+
         }
 
         [Fact]
-        public async Task Api_Product_Get_Coneection_is_ok()
+        public async Task Api_Product_Get_Connection_is_ok()
         {
-            // Act            
-            var response = await _client.GetAsync("/api/product");
-            response.EnsureSuccessStatusCode();
-
-            using var responseStream = await response.Content.ReadAsStreamAsync();
-            var jsonSerializerOptions = new JsonSerializerOptions()
+            using (var client = new TestClientProvider().Client)
             {
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-            };
+                // Act
+                HttpResponseMessage response = await client.GetAsync("/api/product");
+                response.EnsureSuccessStatusCode();
 
-            sut_list_products = await JsonSerializer.DeserializeAsync<List<ProductDto>>(responseStream, jsonSerializerOptions);
-
-            // Assert
-            Assert.True(sut_list_products.Count > 0);
+                // Assert
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            }
         }
 
         [Fact]
         public async Task Api_Product_Get_Returns_List()
         {
-            // Act            
-            var response = await _client.GetAsync("/api/product");
-            response.EnsureSuccessStatusCode();
-
-            using var responseStream = await response.Content.ReadAsStreamAsync();
-            var jsonSerializerOptions = new JsonSerializerOptions()
+            using (var client = new TestClientProvider().Client)
             {
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-            };
+                //Arrange
+                List<ProductDto> sut_list_products;
 
-            sut_list_products = await JsonSerializer.DeserializeAsync<List<ProductDto>>(responseStream, jsonSerializerOptions);
+                // Act
+                HttpResponseMessage response = await client.GetAsync("/api/product");
+                response.EnsureSuccessStatusCode();
 
-            // Assert
-            Assert.True(sut_list_products.Count > 0);
+                using var responseStream = await response.Content.ReadAsStreamAsync();
+                var jsonSerializerOptions = new JsonSerializerOptions()
+                {
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                };
+
+                sut_list_products = await JsonSerializer.DeserializeAsync<List<ProductDto>>(responseStream, jsonSerializerOptions);
+
+                // Assert
+                Assert.True(sut_list_products.Count > 0);
+            }
         }
 
-        [Fact]
-        public async Task Api_Product_Post_Product()
+        [Theory]
+        [InlineData("Bike", "Bike name", "This is the best Bike")]
+        [InlineData("Game", "Game name", "This is the best Game")]
+        [InlineData("Food", "Food name", "This is the best Food")]
+        public async Task Api_Product_Post_Product(string productId, string productName, string productDescription)
         {
-            // Act            
-            var response = await _client.GetAsync("/api/product");
-            response.EnsureSuccessStatusCode();
-
-            using var responseStream = await response.Content.ReadAsStreamAsync();
-            var jsonSerializerOptions = new JsonSerializerOptions()
+            using (var client = new TestClientProvider().Client)
             {
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-            };
+                // Arrange
+                Random rand = new Random();
+                string id = rand.Next(1,99999).ToString();
 
-            sut_list_products = await JsonSerializer.DeserializeAsync<List<ProductDto>>(responseStream, jsonSerializerOptions);
+                ProductDto newProduct = new ProductDto
+                {
+                    ProductId = productId + id,
+                    ProductName = productName + " " + id,
+                    ProductDescription = productDescription + " " + id
+                };
 
-            // Assert
-            Assert.True(sut_list_products.Count > 0);
+                // Act
+                string productDtoJsonFormat = JsonSerializer.Serialize(newProduct);
+                StringContent stringContent = new StringContent(productDtoJsonFormat, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync("/api/product", stringContent);
+
+                // Assert
+                response.EnsureSuccessStatusCode();
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            }
         }
     }
 }
--- XUnitTestProject1/TestClientProvider.cs
+++ XUnitTestProject1/TestClientProvider.cs
@@ -1,21 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ApiRestful.Service;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System;
 using System.Net.Http;
 
 namespace ApiRestful.Test
 {
-    public class TestClientProvider
+    public class TestClientProvider : IDisposable
     {
+        private TestServer server;
         public HttpClient Client { get; private set; }
 
         public TestClientProvider()
         {
-            var server = new TestServer(new WebHostBuilder().UseStartup());
+            server = new TestServer(new WebHostBuilder()
+                   .UseStartup<Startup>());
 
             Client = server.CreateClient();
+        }
+
+        public void Dispose()
+        {
+            server?.Dispose();
+            Client?.Dispose();
         }
     }
 }
